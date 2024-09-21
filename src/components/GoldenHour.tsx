119e8f674ce921de01b352dfcf4691bfd13fd043
Alena Tikhomirova <137419549+AlenaTih@users.noreply.github.com>
2024-09-21 22:35:50 +0300

    Update GoldenHour

--- src/components/GoldenHour.tsx
+++ src/components/GoldenHour.tsx
@@ -1,13 +1,24 @@
-import { useState, ChangeEvent, MouseEvent } from "react"
+import {
+        useState,
+        useEffect,
+        ChangeEvent,
+        MouseEvent
+    } from "react"
+// import axios from "axios"
 
 function GoldenHour() {
     const [formData, setFormData] = useState({
-        location: "",
+        city: "",
     })
 
-    const [sunsetTime, setSunsetTime] = useState(7)
+    const [sunsetTime, setSunsetTime] = useState(0)
 
     const goldenHourTime = sunsetTime - 1
+
+    const [location, setLocation] = useState({
+        longitude: 0,
+        latitude: 0,
+    })
 
     function handleChange(event: ChangeEvent<HTMLInputElement>) {
         const { name, value } = event.target
@@ -32,6 +43,60 @@ function GoldenHour() {
         console.log(currentTime)
     }
 
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(position => {
+            setLocation({
+                longitude: position.coords.longitude,
+                latitude: position.coords.latitude,
+            })
+        })
+
+        console.log(location)
+    }, [])
+
+    // useEffect(() => {
+    //     async () => {
+    //         try {
+    //           const apiKey = "YOUR_API_KEY" // Replace with your OpenWeatherMap API key
+    //           const response = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${formData.city}&limit=1&appid=${apiKey}`)
+              
+    //           if (response.data.length > 0) {
+    //             const { lat, lon } = response.data[0]
+    //             setLocation({ latitude: lat, longitude: lon })
+    //           } else {
+    //             alert("City not found")
+    //           }
+    //         } catch (error) {
+    //           console.error("Error fetching data:", error)
+    //         }
+    //       }
+    // }, [])
+
+    // useEffect(() => {
+    //     // Use the longitude and latitude to get the sunset time from the OpenWeatherMap API
+    //     const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${location.latitude}&lon=${location.longitude}&exclude=minutely,hourly,alerts&appid=${API key}`
+
+    //     fetch(url)
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             const unixSunsetTime = data.current.sunset
+    //             const date = new Date(unixSunsetTime * 1000) // Convert from seconds to milliseconds
+    //             const sunsetTimeInHours = date.getHours()
+    //             setSunsetTime(sunsetTimeInHours)
+    //         })
+    //         .catch(error => {
+    //             console.error(error)
+    //         })
+    //         .finally(() => {
+    //             console.log("It worked!")
+    //         })
+
+    //     return () => {
+    //         console.log("Clean up")
+    //     }
+
+    // }, [formData.city])
+
     return (
         <div className="golden-hour">
             <h2 className="golden-hour-title">
@@ -39,15 +104,15 @@ function GoldenHour() {
             </h2>
 
             <form className="golden-hour-form">
-                <label htmlFor="location-input">
-                    Type in your location
+                <label htmlFor="city-input">
+                    Type in your city
                     <input
-                        className="location-input"
-                        id="location-input"
+                        className="city-input"
+                        id="city-input"
                         type="text"
                         placeholder="Your city"
-                        name="location"
-                        value={formData.location}
+                        name="city"
+                        value={formData.city}
                         onChange={handleChange}
                     />
                 </label>
@@ -60,13 +125,13 @@ function GoldenHour() {
             </form>
 
             <div className="golden-hour-result-container">
-                <p className="golden-hour-result-text">
-                    You can see the golden hour at {goldenHourTime} pm
-                    - {sunsetTime} pm
-                    {/* {goldenHourTime > 12 ? "pm" : "am"} */}
-                </p>
-                {formData.location && (<p>
-                    in {formData.location}
+                {sunsetTime && goldenHourTime &&
+                    (<p className="golden-hour-result-text">
+                        You can see the golden hour at {goldenHourTime} pm
+                        - {sunsetTime} pm
+                    </p>)}
+                {formData.city && (<p>
+                    in {formData.city}
                 </p>)}
             </div>
         </div>
