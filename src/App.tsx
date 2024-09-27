a5971d0d6a1569a9f8116ecc9d14b4b9ca67851d
Alena Tikhomirova <137419549+AlenaTih@users.noreply.github.com>
2024-09-28 00:43:33 +0300

    Adding Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import GoldenHour from "./components/GoldenHour.tsx"
+import Footer from "./components/Footer.tsx"
 import "./App.css"
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="app-container">
       <h1 className="golden-hour-title">Golden Hour App</h1>
       <GoldenHour />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+function Footer () {
+    return (
+        <footer className="footer">
+            <p className="footer-text">
+                Photos are by talented creators 
+                on <a
+                        className="footer-link"
+                        href="https://unsplash.com/"
+                        target="_blank">
+                        Unsplash
+                    </a>
+            </p>
+        </footer>
+    )
+}
+
+export default Footer
