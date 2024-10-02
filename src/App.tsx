6ab15db9dd4f6e62db7c68ac852939b59968d303
Alena Tikhomirova <137419549+AlenaTih@users.noreply.github.com>
2024-10-03 00:25:30 +0300

    Optimizing loading of the background image

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import Header from "./components/Header.tsx"
 import GoldenHour from "./components/GoldenHour.tsx"
 import Footer from "./components/Footer.tsx"
+import BackgroundImage from "./assets/cold-sunset-background.png"
 import "./App.css"
 
 function App() {
   return (
-    <div className="app-container">
+    <div
+      className="app-container"
+      style={{ backgroundImage: `url(${BackgroundImage})`}}
+    >
       <Header />
       <GoldenHour />
       <Footer />
